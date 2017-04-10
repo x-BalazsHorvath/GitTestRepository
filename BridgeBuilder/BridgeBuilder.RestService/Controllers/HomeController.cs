e9ac9778c02032d0d8ff27448eec36f75bbac625
x-BalazsHorvath <x-Balazs.Horvath@wizzair.com>
2017-04-10 10:45:48 +0200

    Added a new test solution for Bridge Builder.

---
+++ BridgeBuilder/BridgeBuilder.RestService/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace BridgeBuilder.RestService.Controllers
+{
+    public class HomeController: ApiController
+    {
+        public IHttpActionResult GetNameList()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BridgeBuilder/BridgeBuilder.RestService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BridgeBuilder.RestService.WebApiApplication" Language="C#" %>
