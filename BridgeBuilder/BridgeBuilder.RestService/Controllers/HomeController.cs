f0ffd30f10b60b036f9fd8296e03f8867a0684d2
x-BalazsHorvath <x-Balazs.Horvath@wizzair.com>
2017-04-10 10:58:12 +0200

    Added comment

--- BridgeBuilder/BridgeBuilder.RestService/Controllers/HomeController.cs
+++ BridgeBuilder/BridgeBuilder.RestService/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace BridgeBuilder.RestService.Controllers
 {
     public class HomeController: ApiController
     {
+        /// <summary>
+        /// Updated code
+        /// </summary>
+        /// <returns></returns>
         public IHttpActionResult GetNameList()
         {
             return Ok();
